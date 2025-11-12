95cd89f69032bf07938c375f7cda9f23fbaeca95
MorisFili <moris.fili@gmail.com>
2025-11-12 11:49:03 +0100

    GUI update

--- src/main/java/GUI/SettingsWindow.java
+++ src/main/java/GUI/SettingsWindow.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import session.UserSession;
 
@@ -15,19 +16,30 @@ public class SettingsWindow {
 
     private final Button button;
     private final TextField username;
+    private final TextField group;
+    private final TextField ipAddress;
     private UserSession user;
     private final WindowManager windowManager;
     private final Scene scene;
 
     public SettingsWindow(WindowManager windowManager) {
         this.windowManager = windowManager;
-        Label usernameLabel = new Label("Username: ");
+        Label usernameLabel = new Label("Username:  ");
+        Label groupLabel = new Label("Group:        "); // Lazy alignment
+        Label ipLabel = new Label("IP Address: ");
+        groupLabel.setFont(new Font(15));
         usernameLabel.setFont(new Font(15));
+        ipLabel.setFont(new Font(15));
         username = new TextField();
+        group = new TextField("default");
+        ipAddress = new TextField("0.0.0.0:0000");
         button = new Button("OK");
         HBox hBox = new HBox(usernameLabel, username, button);
+        HBox hBoxM = new HBox(groupLabel, group);
+        HBox hBoxU = new HBox(ipLabel, ipAddress);
+        VBox vBox = new VBox(hBoxU, hBoxM, hBox);
         BorderPane root = new BorderPane();
-        root.setCenter(hBox);
+        root.setCenter(vBox);
         root.setPadding(new Insets(3));
 
         scene = new Scene(root);
@@ -40,9 +52,15 @@ public class SettingsWindow {
 
         // Button click
         button.setOnAction(x -> {
-            // Parse logic, null checks etc
-            user = new UserSession(username.getText());
-            windowManager.showChat(user);
+            try {
+                String[] ipPort = ipAddress.getText().split(":");
+                int ip = Integer.parseInt(ipPort[0]);
+                int port = Integer.parseInt(ipPort[1]);
+                user = new UserSession(username.getText(), group.getText(), ip, port);
+                windowManager.showChat(user);
+            }catch (NumberFormatException e) {
+                System.out.println("Invalid host address format -> " + e.getMessage());
+            }
         });
     }
 
--- src/main/java/session/UserSession.java
+++ src/main/java/session/UserSession.java
@@ -5,12 +5,22 @@ public class UserSession {
     // All runtime-info här
 
     private final String username;
+    private final String group;
+    private final int ip;
+    private final int port;
 
-    public UserSession(String username) {
+    public UserSession(String username, String group, int ip, int port) {
         this.username = username;
+        this.group = group;
+        this.ip = ip;
+        this.port = port;
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public String getGroup() {
+        return group;
     }
 }
