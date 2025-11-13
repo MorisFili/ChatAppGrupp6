42f660b7e0e09c88ffac8ab3b0c4e64e7908b8c2
Hussein <hossein.abrahim2217@gmail.com>
2025-11-13 22:14:28 +0100

    Delete message, somehow it works

--- src/main/java/database/IMessageRepository.java
+++ src/main/java/database/IMessageRepository.java
@@ -2,9 +2,7 @@ package database;
 
 import model.TextNode;
 
-// Har functional interface annotation för att det är bara en metod just nu
-// Om det blir fler metoder senare kommer jag ta bort den
-@FunctionalInterface
 public interface IMessageRepository {
     void saveMessage(TextNode message);
+    void deleteMessage(String username, String content) throws Exception;
 }
