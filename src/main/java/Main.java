b34e0c34d4fc4980bb064d659544431797780f06
MorisFili <moris.fili@gmail.com>
2025-11-14 10:31:36 +0100

    Merge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,7 @@
 import GUI.WindowManager;
-import database.MessageRepository;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import model.TextNode;
 
-import java.time.LocalDateTime;
-import java.util.Scanner;
 
 public class Main extends Application {
     public static void main(String[] args) { launch(); }
@@ -13,5 +9,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         WindowManager windowManager = new WindowManager(stage);
+
+
     }
 }
--- src/main/java/network/NetworkUser.java
+++ src/main/java/network/NetworkUser.java
@@ -1,3 +1,4 @@
+package network;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
