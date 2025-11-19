52ff19383d5c2dcd2b511d23a26b4ef197a4f284
MorisFili <83581740+MorisFili@users.noreply.github.com>
2025-11-19 16:29:47 +0100

    Delete src/main/java/database directory

--- src/main/java/database/IMessageRepository.java
+++
@@ -1,8 +0,0 @@
-package database;
-
-import model.TextNode;
-
-public interface IMessageRepository {
-    void saveMessage(TextNode message);
-    void deleteMessage(String username, String content) throws Exception;
-}
